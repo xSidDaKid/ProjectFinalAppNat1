d57caa6cdb6a4781fa9c46b2d5230e692f686de6
Shajaan <shajaan_71@hotmail.com>
2021-12-12 22:57:28 -0500

    Ajout de la methode login dans le controlleur de Login

---
+++ AppNatProjetSession/src/main/java/com/example/appnatprojetsession/Controllers/LoginController.java
@@ -0,0 +1,93 @@
+package com.example.appnatprojetsession.Controllers;
+
+import com.example.appnatprojetsession.Models.Client;
+import com.example.appnatprojetsession.Models.GestionnaireGuichet;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * @Cours: Applications natives 1
+ * @Remis_à: Toufik Bellal
+ * @Date_de_remise: 16 decembre 2021
+ * @author: A. Alperen, B. Shajaan et I. Gafran
+ */
+public class LoginController {
+
+    public static String codeUtilisateur = "";
+    public static String nipUtilisateur = "";
+    //Login
+    @FXML
+    TextField code;
+    @FXML
+    TextField nip;
+
+    /**
+     * Login avec enter
+     *
+     * @param ae
+     * @throws IOException
+     */
+    @FXML
+    public void onEnter(ActionEvent ae) throws IOException {
+        Login();
+    }
+
+    /**
+     * Methode qui sert a se connecter en tant que Administrateur ou Client
+     *
+     * @throws IOException
+     */
+    public void Login() throws IOException {
+
+        codeUtilisateur = code.getText();
+        nipUtilisateur = nip.getText();
+
+        if (codeUtilisateur.trim().equalsIgnoreCase("") || nipUtilisateur.equalsIgnoreCase("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "S.V.P. Remplir tous les champs");
+            alert.showAndWait();
+        } else if (codeUtilisateur.equalsIgnoreCase("admin") && nipUtilisateur.equalsIgnoreCase("0000")) {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/appnatprojetsession/Admin/menuAdmin.fxml"));
+            Scene first = code.getScene();
+            ((Stage) first.getWindow()).setTitle("Admin");
+            first.setRoot(root);
+        } else {
+            int codeCli = 0, nip = 0;
+            try {
+                codeCli = Integer.parseInt(codeUtilisateur);
+                nip = Integer.parseInt(nipUtilisateur);
+            } catch (Exception e) {
+                System.out.println(e);
+                Alert alert = new Alert(Alert.AlertType.ERROR, "S.V.P. Entrez des numeros");
+                alert.showAndWait();
+                return;
+            }
+
+            Client client = new Client(codeCli, nip);
+            GestionnaireGuichet gg = new GestionnaireGuichet();
+            ArrayList<Client> clients = gg.getClients();
+            System.out.println("HERE 1");
+            for (Client c : clients) {
+                if (client.getCodeClient() == c.getCodeClient()) {
+                    if (c.getNumeroNIP() == client.getNumeroNIP()) {
+                        Parent root = FXMLLoader.load(getClass().getResource("/com/example/appnatprojetsession/User/menuUser.fxml"));
+                        Scene first = code.getScene();
+                        ((Stage) first.getWindow()).setTitle("Client");
+                        first.setRoot(root);
+                    } else {
+                        Alert alert = new Alert(Alert.AlertType.WARNING, "Client non trouvable");
+                        alert.showAndWait();
+                    }
+                }
+            }
+        }
+    }
+}
