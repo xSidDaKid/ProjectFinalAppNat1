9c039ee915470f3315658fce449dcd5225a9dc0e
Shajaan <shajaan_71@hotmail.com>
2021-12-12 22:58:44 -0500

    Ajout du controlleur UserControlleur pour les interfaces utilisateur

---
+++ AppNatProjetSession/src/main/java/com/example/appnatprojetsession/Controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.example.appnatprojetsession.Controllers;
+
+/**
+ * @Cours: Applications natives 1
+ * @Remis_à: Toufik Bellal
+ * @Date_de_remise: 16 decembre 2021
+ * @author: A. Alperen, B. Shajaan et I. Gafran
+ */
+public class UserController {
+    //TODO: Interfaces utilisateur
+}
