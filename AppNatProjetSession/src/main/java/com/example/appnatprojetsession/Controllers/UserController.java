c8c1dd07f8e39292463684bdc2bc7dae0282bbc9
altay27 <1867143@crosemont.qc.ca>
2021-12-16 20:32:26 -0500

    fin transfert et paiement

--- AppNatProjetSession/src/main/java/com/example/appnatprojetsession/Controllers/UserController.java
+++ AppNatProjetSession/src/main/java/com/example/appnatprojetsession/Controllers/UserController.java
@@ -349,7 +349,40 @@ public class UserController implements Initializable {
                         double solde = compteCheque.getSoldeCompte();
 
                         if (solde < montant + 1.25) {
-                            Alert alert = new Alert(Alert.AlertType.ERROR, "Votre solde n'est pas suffisant");
+                            boolean validation=false;
+                            int idUser = Integer.parseInt(LoginController.codeUtilisateur);
+                            ArrayList<Marge> listeMarge = GestionnaireGuichet.getComptesMarge();
+                            for (int j = 0; j < listeMarge.size(); j++){
+                                if(listeMarge.get(j).getCodeClient()==idUser){
+
+                                    double soldeTemp=montant-solde;
+
+                                    Marge compteMarge= listeMarge.get(j);
+                                    compteMarge.setSoldeCompte(compteMarge.getSoldeCompte()+soldeTemp+1.25);
+                                    listeMarge.set(j,compteMarge);
+
+                                    GestionnaireGuichet.setComptesMarge(listeMarge);
+
+
+                                    compteCheque.setSoldeCompte(0.0);
+                                    System.out.println(compteCheque.getSoldeCompte());
+                                    listeCheque.set(i, compteCheque);
+
+                                    GestionnaireGuichet.setComptesCheque(listeCheque);
+                                    validation=true;
+
+                                }
+
+
+
+                            }
+
+                            if(validation==false){
+                                Alert alert = new Alert(Alert.AlertType.ERROR, "Solde insuffisant. Vous n'avez pas de compte de marge");
+                                alert.showAndWait();
+                            }
+
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION, "LE montant qui depasse votre solde à été ajouté comme facture dans votre compte de marge");
                             alert.showAndWait();
                             return;
                         }
@@ -382,7 +415,39 @@ public class UserController implements Initializable {
                         double solde = compteCheque.getSoldeCompte();
 
                         if (solde < montant + 1.25) {
-                            Alert alert = new Alert(Alert.AlertType.ERROR, "Votre solde n'est pas suffisant");
+                            boolean validation=false;
+
+                            int idUser = Integer.parseInt(LoginController.codeUtilisateur);
+                            ArrayList<Marge> listeMarge = GestionnaireGuichet.getComptesMarge();
+                            for (int j = 0; j < listeMarge.size(); j++){
+                                if(listeMarge.get(j).getCodeClient()==idUser){
+
+                                    double soldeTemp=montant-solde;
+
+                                    Marge compteMarge= listeMarge.get(j);
+                                    compteMarge.setSoldeCompte(compteMarge.getSoldeCompte()+soldeTemp+1.25);
+                                    listeMarge.set(j,compteMarge);
+
+                                    GestionnaireGuichet.setComptesMarge(listeMarge);
+
+
+                                    compteCheque.setSoldeCompte(0.0);
+                                    System.out.println(compteCheque.getSoldeCompte());
+                                    listeCheque.set(i, compteCheque);
+
+                                    GestionnaireGuichet.setComptesEpargne(listeCheque);
+                                    validation=true;
+
+                                }
+
+
+                            }
+                            if(validation==false){
+                                Alert alert = new Alert(Alert.AlertType.ERROR, "Solde insuffisant. Vous n'avez pas de compte de marge");
+                                alert.showAndWait();
+                            }
+
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION, "LE montant qui depasse votre solde à été ajouté comme facture dans votre compte de marge");
                             alert.showAndWait();
                             return;
                         }
@@ -395,7 +460,7 @@ public class UserController implements Initializable {
                         GestionnaireGuichet.setComptesEpargne(listeCheque);
 
                         gg.getBanque().retraitGuichet(montant);
-                        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Un solde de " + montant + " a été ajouté");
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Un solde de " + montant + " a été retiré");
                         alert.showAndWait();
                     }
 
@@ -453,7 +518,7 @@ public class UserController implements Initializable {
         if (radioTranfert.isSelected()) {
             transfert();
         } else if (radioPaiement.isSelected()) {
-            retraitCompte();
+            paiement();
         }
     }
 
@@ -548,16 +613,16 @@ public class UserController implements Initializable {
                         GestionnaireGuichet.setComptesEpargne(liste);
 
                     } else if (typeUser.equals("Hypothécaire")) {
-                        Epargne epargne = (Epargne) listeCheque.get(i);
-                        ArrayList<Epargne> liste = GestionnaireGuichet.getComptesEpargne();
+                        Hypothécaire epargne = (Hypothécaire) listeCheque.get(i);
+                        ArrayList<Hypothécaire> liste = GestionnaireGuichet.getComptesHypothecaire();
                         liste.set(i, epargne);
-                        GestionnaireGuichet.setComptesEpargne(liste);
+                        GestionnaireGuichet.setComptesHypothecaire(liste);
 
                     } else if (typeUser.equals("Marge")) {
-                        Epargne epargne = (Epargne) listeCheque.get(i);
-                        ArrayList<Epargne> liste = GestionnaireGuichet.getComptesEpargne();
+                        Marge epargne = (Marge) listeCheque.get(i);
+                        ArrayList<Marge> liste = GestionnaireGuichet.getComptesMarge();
                         liste.set(i, epargne);
-                        GestionnaireGuichet.setComptesEpargne(liste);
+                        GestionnaireGuichet.setComptesMarge(liste);
                     }
 
 
@@ -573,6 +638,137 @@ public class UserController implements Initializable {
         }
 
     }
+
+    public void paiement(){
+
+        if (inputPaiement.getText().equalsIgnoreCase("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "S.V.P. veuillez remplir les champs");
+            alert.showAndWait();
+            return;
+        }
+        double montantTransfert = Double.parseDouble(inputPaiement.getText());
+        String compte = transfertComptes.getSelectionModel().getSelectedItem();
+
+
+
+        System.out.println(compte);
+        String[] typeList = compte.split(":");
+        String type = typeList[1].trim();
+        System.out.println(type + "test");
+        compte = typeList[2];
+
+        if (!type.equals("Marge")){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Vous pouvez payer seuleument votre facture de Marge");
+            alert.showAndWait();
+            return;
+        }
+
+
+
+        Compte compteCheque;
+        ArrayList<Compte> listeCheque = new ArrayList<>();
+        listeCheque.addAll(GestionnaireGuichet.getComptesMarge());
+
+
+        Cheque compteChequeUser;
+        ArrayList<Cheque> listeChequeUser = new ArrayList<>();
+        listeChequeUser = (GestionnaireGuichet.getComptesCheque());
+
+
+        for (int i = 0; i < listeChequeUser.size(); i++) {
+
+            if (listeChequeUser.get(i).getCodeClient() == Integer.parseInt(LoginController.codeUtilisateur)) {
+                compteChequeUser = listeChequeUser.get(i);
+                System.out.println(compteChequeUser.getSoldeCompte());
+                double solde = compteChequeUser.getSoldeCompte();
+
+
+                if (solde < montantTransfert + 1.25) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Votre solde n'est pas suffisant");
+                    alert.showAndWait();
+                    return;
+                }
+
+
+                solde = (solde - montantTransfert);
+                compteChequeUser.setSoldeCompte(solde);
+                System.out.println(compteChequeUser.getSoldeCompte());
+                listeChequeUser.set(i, compteChequeUser);
+
+                GestionnaireGuichet.setComptesCheque(listeChequeUser);
+
+
+            }
+
+        }
+
+
+        for (int i = 0; i < listeCheque.size(); i++) {
+            System.out.println("test boucle");
+            if (listeCheque.get(i).getNumeroCompte() == Integer.parseInt(compte)) {
+
+                compteCheque = listeCheque.get(i);
+                System.out.println(compteCheque.getSoldeCompte());
+                double solde = compteCheque.getSoldeCompte();
+
+
+
+                if(montantTransfert==compteCheque.getSoldeCompte()){
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Vous avez une facture de "+solde+" a payer. (Veuillez entrer le montant exact)");
+                    alert.showAndWait();
+                    return;
+
+                }
+
+
+                solde = (solde - montantTransfert);
+                compteCheque.setSoldeCompte(solde);
+                System.out.println(compteCheque.getSoldeCompte());
+                listeCheque.set(i, compteCheque);
+
+
+                for (int j = 0; j < listeCheque.size(); j++) {
+
+                    System.out.println(compte);
+                    String[] typeListUser = listeCheque.get(j).getClass().toString().split(":");
+                    String typeUser = typeList[1].trim();
+                    System.out.println(type + "test");
+                    compte = typeList[2];
+
+
+                    if (typeUser.equals("Epargne")) {
+                        Epargne epargne = (Epargne) listeCheque.get(i);
+                        ArrayList<Epargne> liste = GestionnaireGuichet.getComptesEpargne();
+                        liste.set(i, epargne);
+                        GestionnaireGuichet.setComptesEpargne(liste);
+
+                    } else if (typeUser.equals("Hypothécaire")) {
+                        Hypothécaire epargne = (Hypothécaire) listeCheque.get(i);
+                        ArrayList<Hypothécaire> liste = GestionnaireGuichet.getComptesHypothecaire();
+                        liste.set(i, epargne);
+                        GestionnaireGuichet.setComptesHypothecaire(liste);
+
+                    } else if (typeUser.equals("Marge")) {
+                        Marge epargne = (Marge) listeCheque.get(i);
+                        ArrayList<Marge> liste = GestionnaireGuichet.getComptesMarge();
+                        liste.set(i, epargne);
+                        GestionnaireGuichet.setComptesMarge(liste);
+                    }
+
+
+                }
+
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Paiement effectué");
+                alert.showAndWait();
+                return;
+
+            }
+
+        }
+
+    }
+
 
     /**
      * Methode qui sert a retourner au menu User
